4704bab7cc6f73d8b6f65c8f051beb4c95eee179
Phan Nguyen Khai <phan.nkhai05@gmail.com>
2024-01-10 08:15:29 +0100

    UI patch

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -162,7 +162,7 @@ export default function GuessTheFlag() {
                 {country.languages ? (
                   <div className="w-full grid grid-cols-2 gap-4">
                     <span className="text-secondary text-md break-words">
-                      Language(s) spoken:
+                      1 of the language(s) spoken:
                     </span>
                     <input
                       name="language-answer"
